dc1829b7916498f10cec6bb36293377240163ec5
Ajay Katte <ajaymkatte95@gmail.com>
2018-01-22 21:09:56 +0530

    better ui

--- app.py
+++ app.py
@@ -8,15 +8,35 @@ env = Environment(loader=FileSystemLoader('html'))
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
+        par = parse.Parse()                             #init
+        equity = par.obtain()                           #fetch parsed values
+
+        last_update = par.last_update()                 #fetch last update date
+
+        unidict = []
+        for entry in equity:
+            unidict.append({key.decode('utf8'): value.decode('utf8') for key, value in entry.items()})          #decode
 
         tmpl = env.get_template('index.html')
-        return tmpl.render()
+        return tmpl.render(equity = unidict, last_update = last_update, heading = "Top ten entries")
 
     @cherrypy.expose
     def search(self, query = None):
+        par = parse.Parse()                             #init
+        equity = par.search(query.upper().rstrip())     #fetch parsed search file
+
+        last_update = par.last_update()                 #fetch last update date
+
+        print("\n\n\n")
+        print (equity)
+        print("\n\n\n")
+        unidict = []
+
+        if len(equity) > 0:
+            unidict.append({key.decode('utf8'): value.decode('utf8') for key, value in equity.items()})         #decode
 
         tmpl = env.get_template('search.html')
-        return tmpl.render()
+        return tmpl.render(equity = unidict, query = query, last_update = last_update, heading = ("Results for " + query) if len(unidict) > 0 else "No matches found")
 
 config = {
     'global': {
